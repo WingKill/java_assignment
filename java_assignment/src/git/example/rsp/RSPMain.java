6977d8cb11ccbbc657eb772f014befa5ea622804
601-16 <601-16@DESKTOP-J2TK4C1>
2024-01-16 17:36:17 +0900

    commit assignment : 과제라고 과제

---
+++ java_assignment/src/git/example/rsp/RSPMain.java
@@ -0,0 +1,30 @@
+package git.example.rsp;
+
+import java.util.Scanner;
+
+public class RSPMain {
+
+	public static void main(String[] args) {
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("가위 바위 보를 입력하세요");
+			String rsp = sc.next();
+			
+			Player user = new Player(rsp); // 요청하신 함수 생성 방법
+			Player computer = new Player();
+			
+			computer.getResult(user);
+			
+			System.out.println("계속 : Y / 중단 : N");
+			
+			char ch = sc.next().charAt(0);
+			
+			if(ch == 'N' || ch == 'n')
+				break;
+		}	
+		System.out.println("게임이 종료되었습니다");
+
+	}
+
+}
---
+++ java_assignment/src/git/mywork/rsp/PlayRsp.java
@@ -0,0 +1,10 @@
+package git.mywork.rsp;
+
+public class PlayRsp {
+
+	public static void main(String[] args) {
+		RspGame rsp = new RspGame();
+		rsp.mustStart();
+	}
+
+}
